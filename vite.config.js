4d3fb0fed8fb3512f597fb9a1b199e18a7f57bdf
Annie Huang <annie.hn@gmail.com>
2024-04-25 16:21:54 +1000

    Update rollup-plugin-visualizer to anlysis bundle size for vite app

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
+// https://stackoverflow.com/questions/75746767/is-there-any-bundle-analyzer-for-vite
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      template: 'treemap', // or sunburst
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+      filename: 'analyse.html', // will be saved in project's root
+    }),
+  ],
 });
